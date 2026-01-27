4c30ceb9a3e975399fede3f1be60195df9f0ae29
Siddharth Balaji <siddharthbalaji6@gmail.com>
2026-01-26 23:43:52 -0800

    feat: added containers tab for visualization

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const SYSTEM_TAB_LABEL = isMacOS ? "macOS" : "System";
 const SYSTEM_TAB_ID = "__system__";
 
 // View modes
-type ViewMode = "graph" | "api-tester";
+type ViewMode = "graph" | "containers" | "api-tester";
 
 function App() {
   const { snapshot, loading, error } = useSystemSnapshot(2000);
@@ -123,6 +123,32 @@ function App() {
     };
   }, [graph.nodes, graph.edges, ports, selectedTab]);
 
+  // Filter to show only Docker containers
+  const dockerContainerData = useMemo(() => {
+    // Get only Docker container nodes
+    const containerNodes = graph.nodes.filter((node) => node.isDockerContainer);
+    const containerNodeIds = new Set(containerNodes.map((n) => n.id));
+
+    // Filter edges to only include those between Docker containers
+    const containerEdges = graph.edges.filter(
+      (edge) =>
+        containerNodeIds.has(edge.source) && containerNodeIds.has(edge.target),
+    );
+
+    // Get ports from containers
+    const containerPorts = new Set<number>();
+    containerNodes.forEach((node) => {
+      node.ports.forEach((p) => containerPorts.add(p.port));
+    });
+    const filteredPorts = ports.filter((p) => containerPorts.has(p.port));
+
+    return {
+      nodes: containerNodes,
+      edges: containerEdges,
+      ports: filteredPorts,
+    };
+  }, [graph.nodes, graph.edges, ports]);
+
   return (
     <div className="app">
       {/* App Title */}
@@ -163,6 +189,23 @@ function App() {
             </svg>
           </span>
           Service Map
+        </button>
+        <button
+          className={`view-mode-tab ${viewMode === "containers" ? "view-mode-tab-active" : ""}`}
+          onClick={() => setViewMode("containers")}
+        >
+          <span className="view-mode-icon">
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M13.983 11.078h2.119a.186.186 0 00.186-.185V9.006a.186.186 0 00-.186-.186h-2.119a.185.185 0 00-.185.185v1.888c0 .102.083.185.185.185m-2.954-5.43h2.118a.186.186 0 00.186-.186V3.574a.186.186 0 00-.186-.185h-2.118a.185.185 0 00-.185.185v1.888c0 .102.082.185.185.186m0 2.716h2.118a.187.187 0 00.186-.186V6.29a.186.186 0 00-.186-.185h-2.118a.185.185 0 00-.185.185v1.887c0 .102.082.185.185.186m-2.93 0h2.12a.186.186 0 00.184-.186V6.29a.185.185 0 00-.185-.185H8.1a.185.185 0 00-.185.185v1.887c0 .102.083.185.185.186m-2.964 0h2.119a.186.186 0 00.185-.186V6.29a.185.185 0 00-.185-.185H5.136a.186.186 0 00-.186.185v1.887c0 .102.084.185.186.186m5.893 2.715h2.118a.186.186 0 00.186-.185V9.006a.186.186 0 00-.186-.186h-2.118a.185.185 0 00-.185.185v1.888c0 .102.082.185.185.185m-2.93 0h2.12a.185.185 0 00.184-.185V9.006a.185.185 0 00-.184-.186h-2.12a.185.185 0 00-.184.185v1.888c0 .102.083.185.185.185m-2.964 0h2.119a.185.185 0 00.185-.185V9.006a.185.185 0 00-.185-.186h-2.119a.185.185 0 00-.186.185v1.888c0 .102.084.185.186.185m-2.92 0h2.12a.185.185 0 00.184-.185V9.006a.185.185 0 00-.184-.186h-2.12a.186.186 0 00-.186.186v1.887c0 .102.084.185.186.185m-2.929 0h2.119a.185.185 0 00.185-.185V9.006a.186.186 0 00-.185-.186h-2.12a.185.185 0 00-.184.185v1.888c0 .102.083.185.185.185M23.763 9.89c-.065-.051-.672-.51-1.954-.51-.338.001-.676.03-1.01.087-.248-1.7-1.653-2.53-1.716-2.566l-.344-.199-.226.327c-.284.438-.49.922-.612 1.43-.23.97-.09 1.882.403 2.661-.595.332-1.55.413-1.744.42H.751a.751.751 0 00-.75.748 11.376 11.376 0 00.692 4.062c.545 1.428 1.355 2.48 2.41 3.124 1.18.723 3.1 1.137 5.275 1.137.983.003 1.963-.086 2.93-.266a12.248 12.248 0 003.823-1.389c.98-.567 1.86-1.288 2.61-2.136 1.252-1.418 1.998-2.997 2.553-4.4h.221c1.372 0 2.215-.549 2.68-1.009.309-.293.55-.65.707-1.046l.098-.288Z"/>
+            </svg>
+          </span>
+          Containers
         </button>
         <button
           className={`view-mode-tab ${viewMode === "api-tester" ? "view-mode-tab-active" : ""}`}
@@ -239,6 +282,46 @@ function App() {
               <ServiceSidebar
                 nodes={filteredData.nodes}
                 ports={filteredData.ports}
+                loading={loading}
+                onTestService={handleTestService}
+              />
+            </div>
+          </>
+        ) : viewMode === "containers" ? (
+          <>
+            {/* Docker Containers Graph */}
+            <div className="graph-container">
+              {loading ? (
+                <div className="loading">Scanning Docker containers...</div>
+              ) : dockerContainerData.nodes.length === 0 ? (
+                <div className="graph-empty">
+                  <div className="docker-empty-icon">
+                    <svg
+                      width="48"
+                      height="48"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      opacity="0.3"
+                    >
+                      <path d="M13.983 11.078h2.119a.186.186 0 00.186-.185V9.006a.186.186 0 00-.186-.186h-2.119a.185.185 0 00-.185.185v1.888c0 .102.083.185.185.185m-2.954-5.43h2.118a.186.186 0 00.186-.186V3.574a.186.186 0 00-.186-.185h-2.118a.185.185 0 00-.185.185v1.888c0 .102.082.185.185.186m0 2.716h2.118a.187.187 0 00.186-.186V6.29a.186.186 0 00-.186-.185h-2.118a.185.185 0 00-.185.185v1.887c0 .102.082.185.185.186m-2.93 0h2.12a.186.186 0 00.184-.186V6.29a.185.185 0 00-.185-.185H8.1a.185.185 0 00-.185.185v1.887c0 .102.083.185.185.186m-2.964 0h2.119a.186.186 0 00.185-.186V6.29a.185.185 0 00-.185-.185H5.136a.186.186 0 00-.186.185v1.887c0 .102.084.185.186.186m5.893 2.715h2.118a.186.186 0 00.186-.185V9.006a.186.186 0 00-.186-.186h-2.118a.185.185 0 00-.185.185v1.888c0 .102.082.185.185.185m-2.93 0h2.12a.185.185 0 00.184-.185V9.006a.185.185 0 00-.184-.186h-2.12a.185.185 0 00-.184.185v1.888c0 .102.083.185.185.185m-2.964 0h2.119a.185.185 0 00.185-.185V9.006a.185.185 0 00-.185-.186h-2.119a.185.185 0 00-.186.185v1.888c0 .102.084.185.186.185m-2.92 0h2.12a.185.185 0 00.184-.185V9.006a.185.185 0 00-.184-.186h-2.12a.186.186 0 00-.186.186v1.887c0 .102.084.185.186.185m-2.929 0h2.119a.185.185 0 00.185-.185V9.006a.186.186 0 00-.185-.186h-2.12a.185.185 0 00-.184.185v1.888c0 .102.083.185.185.185M23.763 9.89c-.065-.051-.672-.51-1.954-.51-.338.001-.676.03-1.01.087-.248-1.7-1.653-2.53-1.716-2.566l-.344-.199-.226.327c-.284.438-.49.922-.612 1.43-.23.97-.09 1.882.403 2.661-.595.332-1.55.413-1.744.42H.751a.751.751 0 00-.75.748 11.376 11.376 0 00.692 4.062c.545 1.428 1.355 2.48 2.41 3.124 1.18.723 3.1 1.137 5.275 1.137.983.003 1.963-.086 2.93-.266a12.248 12.248 0 003.823-1.389c.98-.567 1.86-1.288 2.61-2.136 1.252-1.418 1.998-2.997 2.553-4.4h.221c1.372 0 2.215-.549 2.68-1.009.309-.293.55-.65.707-1.046l.098-.288Z"/>
+                    </svg>
+                  </div>
+                  <p>No Docker containers running</p>
+                  <span>Start some containers to see them here</span>
+                </div>
+              ) : (
+                <GraphView
+                  nodes={dockerContainerData.nodes}
+                  edges={dockerContainerData.edges}
+                />
+              )}
+            </div>
+
+            {/* Sidebar for containers */}
+            <div className="sidebar">
+              <ServiceSidebar
+                nodes={dockerContainerData.nodes}
+                ports={dockerContainerData.ports}
                 loading={loading}
                 onTestService={handleTestService}
               />
